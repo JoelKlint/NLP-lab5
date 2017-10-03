4f625c15dddf94b1149e0850e640a48b4f7bf4b1
Joel Klint <joel.klint@gmail.com>
2017-10-03 15:00:39 +0200

    Finished lab.. kind of

--- dparser.py
+++ dparser.py
@@ -5,7 +5,13 @@ __author__ = "Pierre Nugues"
 
 import transition
 import conll
+import features
 
+import time
+from sklearn import linear_model
+from sklearn import tree
+from sklearn import metrics
+from sklearn.feature_extraction import DictVectorizer
 
 def reference(stack, queue, graph):
     """
@@ -44,6 +50,46 @@ def reference(stack, queue, graph):
     return stack, queue, graph, 'sh'
 
 
+def encode_classes(y_symbols):
+    """
+    Encode the classes as numbers
+    :param y_symbols:
+    :return: the y vector and the lookup dictionaries
+    """
+    # We extract the chunk names
+    classes = sorted(list(set(y_symbols)))
+    """
+    Results in:
+    ['B-ADJP', 'B-ADVP', 'B-CONJP', 'B-INTJ', 'B-LST', 'B-NP', 'B-PP',
+    'B-PRT', 'B-SBAR', 'B-UCP', 'B-VP', 'I-ADJP', 'I-ADVP', 'I-CONJP',
+    'I-INTJ', 'I-NP', 'I-PP', 'I-PRT', 'I-SBAR', 'I-UCP', 'I-VP', 'O']
+    """
+    # We assign each name a number
+    dict_classes = dict(enumerate(classes))
+    """
+    Results in:
+    {0: 'B-ADJP', 1: 'B-ADVP', 2: 'B-CONJP', 3: 'B-INTJ', 4: 'B-LST',
+    5: 'B-NP', 6: 'B-PP', 7: 'B-PRT', 8: 'B-SBAR', 9: 'B-UCP', 10: 'B-VP',
+    11: 'I-ADJP', 12: 'I-ADVP', 13: 'I-CONJP', 14: 'I-INTJ',
+    15: 'I-NP', 16: 'I-PP', 17: 'I-PRT', 18: 'I-SBAR',
+    19: 'I-UCP', 20: 'I-VP', 21: 'O'}
+    """
+
+    # We build an inverted dictionary
+    inv_dict_classes = {v: k for k, v in dict_classes.items()}
+    """
+    Results in:
+    {'B-SBAR': 8, 'I-NP': 15, 'B-PP': 6, 'I-SBAR': 18, 'I-PP': 16, 'I-ADVP': 12,
+    'I-INTJ': 14, 'I-PRT': 17, 'I-CONJP': 13, 'B-ADJP': 0, 'O': 21,
+    'B-VP': 10, 'B-PRT': 7, 'B-ADVP': 1, 'B-LST': 4, 'I-UCP': 19,
+    'I-VP': 20, 'B-NP': 5, 'I-ADJP': 11, 'B-CONJP': 2, 'B-INTJ': 3, 'B-UCP': 9}
+    """
+
+    # We convert y_symbols into a numerical vector
+    y = [inv_dict_classes[i] for i in y_symbols]
+    return y, dict_classes, inv_dict_classes
+
+
 if __name__ == '__main__':
     train_file = 'corpus/sv/swedish_talbanken05_train.conll'
     test_file = 'corpus/sv/swedish_talbanken05_test_blind.conll'
@@ -53,7 +99,48 @@ if __name__ == '__main__':
     sentences = conll.read_sentences(train_file)
     formatted_corpus = conll.split_rows(sentences, column_names_2006)
 
+    feature_names_short = [
+        'stack_0_word',
+        'stack_0_POS',
+        'queue_0_word',
+        'queue_0_POS',
+        'can-re',
+        'can-la'
+    ]
+
+    feature_names_middle = [
+        'stack_0_word',
+        'stack_0_POS',
+        'stack_1_word',
+        'stack_1_POS',
+        'queue_0_word',
+        'queue_0_POS',
+        'queue_1_word',
+        'queue_1_POS',
+        'can-re',
+        'can-la'
+    ]
+
+    feature_names_long = [
+        'stack_0_POS',
+        'stack_0_word',
+        'stack_1_word',
+        'stack_1_POS',
+        'queue_0_word',
+        'queue_0_POS',
+        'after_stack_0_word',
+        'after_stack_0_POS',
+        'can-re',
+        'can-la'
+        'can-ra',
+        'root_of_sentence'
+    ]
+
     sent_cnt = 0
+
+    y_symbols = [] # Our array of transistions
+    X_dict = list() # Our matrix
+
     for sentence in formatted_corpus:
         sent_cnt += 1
         if sent_cnt % 1000 == 0:
@@ -66,26 +153,45 @@ if __name__ == '__main__':
         graph['heads']['0'] = '0'
         graph['deprels'] = {}
         graph['deprels']['0'] = 'ROOT'
-        transitions = []
+
+
         while queue:
+            x = features.extract(stack, queue, graph, feature_names_long, sentence)
+            X_dict.append(x)
+
             stack, queue, graph, trans = reference(stack, queue, graph)
-            transitions.append(trans)
+            
+            y_symbols.append(trans)
         stack, graph = transition.empty_stack(stack, graph)
-        # print('Equal graphs:', transition.equal_graphs(sentence, graph))
-        if transition.equal_graphs(sentence, graph) == False:
-            if(len(transitions) < 15):
-                print("")
-                print("")
-                print(transition.equal_graphs(sentence, graph))
-                print(len(transitions))
-                for word in sentence:
-                    print(word['form'])
-                print(transitions)
-                print(graph)
+
 
 
         # Poorman's projectivization to have well-formed graphs.
         for word in sentence:
             word['head'] = graph['heads'][word['id']]
-        # print(transitions)
+        # print(y_symbols)
         # print(graph)
+
+
+    """
+        Start SKlearn part
+    """
+    vec = DictVectorizer(sparse=True)
+    X = vec.fit_transform(X_dict)
+    y, dict_classes, inv_dict_classes = encode_classes(y_symbols)
+
+    print("Training the model...")
+    training_start_time = time.clock()
+    classifier = linear_model.LogisticRegression(penalty='l2', dual=True, solver='liblinear', verbose=1)
+    # classifier = linear_model.Perceptron(penalty='l2')
+    # classifier = tree.DecisionTreeClassifier()
+    model = classifier.fit(X, y)
+    print(model)
+
+    y_train_predicted = model.predict(X)
+
+    # TODO: Test the model with test data
+
+    print("Classification report for classifier %s:\n%s\n"
+          % (classifier, metrics.classification_report(y_symbols, list(map(lambda y_pred: dict_classes[y_pred], y_train_predicted)) )))
+
---
+++ features.py
@@ -0,0 +1,124 @@
+import transition
+
+# TODO: Should we not use sentence?
+def extract(stack, queue, graph, feature_names, sentence):
+    """
+    Returns a row
+    """
+
+    features = list()
+
+    # TODO: Should we use postag os cpostag?
+    POS_TAG = 'postag'
+    WORD_TAG = 'form'
+    ID_TAG = 'id'
+    DEPREL_TAG = 'deprel'
+    NULL_VALUE = 'nil'
+
+    # stack_0
+    if stack:
+        stack_0_POS = stack[0][POS_TAG]
+        stack_0_word = stack[0][WORD_TAG]
+    else:
+        stack_0_POS = NULL_VALUE
+        stack_0_word = NULL_VALUE
+
+    # stack_1
+    if len(stack) > 1:
+        stack_1_POS = stack[1][POS_TAG]
+        stack_1_word = stack[1][WORD_TAG]
+    else:
+        stack_1_POS = NULL_VALUE
+        stack_1_word = NULL_VALUE
+
+    # queue_0
+    if queue:
+        queue_0_POS = queue[0][POS_TAG]
+        queue_0_word = queue[0][WORD_TAG]
+    else:
+        queue_0_POS = NULL_VALUE
+        queue_0_word = NULL_VALUE
+
+    # queue_1
+    if len(queue) > 1:
+        queue_1_POS = queue[1][POS_TAG]
+        queue_1_word = queue[1][WORD_TAG]
+    else:
+        queue_1_POS = NULL_VALUE
+        queue_1_word = NULL_VALUE
+
+    # word after top of stack in sentence
+    if stack_0_word == NULL_VALUE:
+        after_stack_0_word = NULL_VALUE
+        after_stack_0_POS = NULL_VALUE
+    else:
+        id_stack_0 = int(stack[0]['id'])
+        if len(sentence)-1 == id_stack_0: #stack 0 is the last word
+            after_stack_0_word = NULL_VALUE
+            after_stack_0_POS = NULL_VALUE
+        else:
+            next_word = sentence[id_stack_0+1]
+            after_stack_0_word = next_word[WORD_TAG]
+            after_stack_0_POS = next_word[POS_TAG]
+    
+    # The root word of the sentence
+    for word in sentence:
+        if word[DEPREL_TAG] == 'ROOT' and word[POS_TAG] != 'ROOT':
+            root_word_of_sentence = word[WORD_TAG]
+            break
+
+    if len(feature_names) == 6:
+        features.append(stack_0_word)
+        features.append(stack_0_POS)
+
+        features.append(queue_0_word)
+        features.append(queue_0_POS)
+
+        features.append(transition.can_reduce(stack, graph))
+        features.append(transition.can_leftarc(stack, graph))
+
+    elif len(feature_names) == 10:
+        features.append(stack_0_word)
+        features.append(stack_0_POS)
+
+        features.append(stack_1_word)
+        features.append(stack_1_POS)
+
+        features.append(queue_0_word)
+        features.append(queue_0_POS)
+
+        features.append(queue_1_word)
+        features.append(queue_1_POS)
+
+        features.append(transition.can_reduce(stack, graph))
+        features.append(transition.can_leftarc(stack, graph))
+
+    elif len(feature_names) == 14:
+        features.append(stack_0_word)
+        features.append(stack_0_POS)
+
+        features.append(stack_1_word)
+        features.append(stack_1_POS)
+
+        features.append(queue_0_word)
+        features.append(queue_0_POS)
+
+        features.append(queue_1_word)
+        features.append(queue_1_POS)
+
+        features.append(after_stack_0_word)
+        features.append(after_stack_0_POS)
+
+        features.append(transition.can_reduce(stack, graph))
+        features.append(transition.can_leftarc(stack, graph))
+
+        # Our own features
+        features.append(transition.can_rightarc(stack, graph))
+        features.append(root_word_of_sentence)
+
+
+    # Convert features object
+    features = dict(zip(feature_names, features))
+
+
+    return features
